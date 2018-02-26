b71c79e4046a88af07851c79eb712d9f7c929bba
Charles Ahmadzadeh <superzadeh@outlook.com>
2018-02-26 20:05:43 +0100

    sprint 1

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
 import style from './style.js'
+
+const price = 100
+
 export default class home extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
-      result: 'Hello World!'
+      result: '0'
     }
   }
-  render() {
+  handleQuantityChange = (event) => {
+    const quantity = event.target.value
+    this.setState({ result: quantity * price })
+  }
+  render () {
     const { result } = this.state
     return (
-      <div>
+      <div className="container">
         <h1>Elephant Carpaccio</h1>
         <br />
-        <label htmlFor="quantity">Number of Items</label>
+        <label htmlFor='quantity'>Number of Items</label>
         &nbsp;
-        <input name="quantity" type="text" />
+        <input name='quantity' type='text' onChange={this.handleQuantityChange} />
         <br />
         <div>{result}</div>
         <style global jsx>

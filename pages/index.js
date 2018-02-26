1f6bbba87a1a665ce2e994725e09e81cd0d1e866
Charles Ahmadzadeh <superzadeh@outlook.com>
2018-02-26 20:13:57 +0100

    sprint 2

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,37 @@ import style from './style.js'
 
 const price = 100
 
+const applyDiscount = totalAmount => {
+  if (totalAmount < 1000) {
+    return totalAmount
+  } else { // (totalAmount >= 1000 && totalAmount < 5000) {
+    return totalAmount - totalAmount * 3 / 100
+  }
+}
+
 export default class home extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       result: '0'
     }
   }
-  handleQuantityChange = (event) => {
+
+  handleQuantityChange = event => {
     const quantity = event.target.value
-    this.setState({ result: quantity * price })
+    const amount = quantity * price
+    const discounted = applyDiscount(amount)
+    this.setState({ result: discounted })
   }
-  render () {
+  render() {
     const { result } = this.state
     return (
       <div className="container">
         <h1>Elephant Carpaccio</h1>
         <br />
-        <label htmlFor='quantity'>Number of Items</label>
+        <label htmlFor="quantity">Number of Items</label>
         &nbsp;
-        <input name='quantity' type='text' onChange={this.handleQuantityChange} />
+        <input name="quantity" type="text" onChange={this.handleQuantityChange} />
         <br />
         <div>{result}</div>
         <style global jsx>
